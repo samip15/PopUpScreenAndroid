b585eff63fae838a7e928465007bb5e14c6b2308
Samip Gnyawali <samipgnyawali15@gmail.com>
2021-04-11 13:42:29 +0545

    first commit

---
+++ app/src/main/java/com/example/popupdemoapp/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.popupdemoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    Dialog myDialog;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myDialog = new Dialog(this);
+    }
+    public void showPopUp(View v)
+    {
+        TextView txtClose;
+        Button btnFollow;
+        myDialog.setContentView(R.layout.custom_pop_up);
+        txtClose = myDialog.findViewById(R.id.txtClose);
+        btnFollow = myDialog.findViewById(R.id.btn_follow);
+        txtClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myDialog.dismiss();
+            }
+        });
+        myDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        myDialog.show();
+    }
+}
